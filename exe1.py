143cdeaa96b98636d46a62e7cfbd36f488088ea4
José Adriano <lucidogold@gmail.com>
2021-04-04 14:26:49 -0300

    update

--- exe1.py
+++ exe1.py
@@ -1,10 +1,24 @@
 pessoas = dict()
 listaPessoas = list()
 resposta = "s"
+idade = []
+nomes = []
+generos = []
+
+
 while resposta =="s":
     pessoas['nome'] = str(input('Insira o nome: '))
     pessoas['sexo'] = str(input('Qual o sexo: '))
-    pessoas['idade'] = str(input('Qual a idade: '))
+    pessoas['idade'] = int(input('Qual a idade: '))
     listaPessoas.append(pessoas.copy())
     resposta = str(input('Deseja continuar s/n: '))
    
+for i in listaPessoas:
+   # for n, s, a in i.items():
+    nomes.append(pessoas['nome'].copy())
+    generos.append(pessoas['sexo'].copy())
+    idade.append(pessoas['idade'].copy())
+
+print(idade)
+print(nomes)
+print(generos)
