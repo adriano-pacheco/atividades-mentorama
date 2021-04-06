b4a7fc217bcfbd3c09bde574ed9e50c4774635c5
José Adriano <lucidogold@gmail.com>
2021-04-06 19:23:38 -0300

    update

--- desafio99.py
+++ desafio99.py
@@ -2,15 +2,22 @@
 # maior(), que receba vário parâmetros com valores inteiros.
 
 #Seu programa tem que analizar os valores e dizer qual deles é maior.
-
+def lin():
+    print('~'*65)
 
 def maior(*num):
-    #cont = 0
-    #maior2 = 0 
+    cont = 0
+    maior2 = 0 
     for valor in num:
-        print(valor, end=' ')
-       
+        cont += 1
+        if valor > maior2:
+            maior2 = valor
+    lin()
+    print(f'O maior numero nessa lista é {maior2}, e a quantidade de numeros são {cont} ')
+    lin()
 
-maior(10,20,50,0)
+maior(10,20,50,0,60)
+
+
 
 
