8c8c1e9c54d7582c6a4df258e7192c49bd679571
José Adriano <lucidogold@gmail.com>
2021-04-06 19:35:51 -0300

    update

--- desafio99.py
+++ desafio99.py
@@ -8,15 +8,19 @@ def lin():
 def maior(*num):
     cont = 0
     maior2 = 0 
-    for valor in num:
+    lin()
+    print('A  lista de numeros são ...' ) 
+    for valor in num:         
         cont += 1
+        print(valor,end=' ')
         if valor > maior2:
             maior2 = valor
+    print()
     lin()
     print(f'O maior numero nessa lista é {maior2}, e a quantidade de numeros são {cont} ')
     lin()
 
-maior(10,20,50,0,60)
+maior(10,20,50,0,60,81)
 
 
 
